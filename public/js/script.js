66aac759af4086941a940c07eb0e7e843eb13055
Jerico Aragon <jerico@gmail.com>
2015-10-06 13:47:04 +0800

    Update assets from resourcecontracts

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    $('select').select2({placeholder: "Select", allowClear: true, theme: "classic"});
+    if ($('select').length) {
+        $('select').select2({placeholder: "Select", allowClear: true, theme: "classic"});
+    }
     //slide effect for filter resource
     var sideslider = $('[data-toggle=collapse-side]');
     var sel = sideslider.attr('data-target');
@@ -13,32 +15,32 @@ $(document).ready(function () {
         var containerHeight = $('.side-collapse-container').height();
         $('.side-collapse').height(containerHeight);
 
-        $('input.resource').on('click',function(){
+        $('input.resource').on('click', function () {
             $('#countries > .col-lg-3').addClass('toggle-width');
         });
 
-        if($(window).width() < 1200) {
+        if ($(window).width() < 1200) {
             $(sel2).find('.col-md-3,.col-md-6').toggleClass('col-md-3').toggleClass('col-md-6');
         }
 
-        if($(window).width() < 992) {
+        if ($(window).width() < 992) {
             $(sel2).find('.col-sm-4,.col-sm-6').toggleClass('col-sm-4').toggleClass('col-sm-6');
         }
 
-        if($(window).width() < 768) {
+        if ($(window).width() < 768) {
             $(sel2).find('.col-xs-6,.col-xs-12').toggleClass('col-xs-6').toggleClass('col-xs-12');
         }
 
-        $(window).resize(function(){
-            if($(window).width() < 1200) {
+        $(window).resize(function () {
+            if ($(window).width() < 1200) {
                 $(sel2).find('.col-md-3,.col-md-6').toggleClass('col-md-6').toggleClass('col-md-3');
             }
 
-            if($(window).width() < 992) {
+            if ($(window).width() < 992) {
                 $(sel2).find('.col-sm-4,.col-sm-6').toggleClass('col-sm-4').toggleClass('col-sm-6');
             }
 
-            if($(window).width() < 768) {
+            if ($(window).width() < 768) {
                 $(sel2).find('.col-xs-6,.col-xs-12').toggleClass('col-xs-6').toggleClass('col-xs-12');
             }
         });
@@ -58,18 +60,16 @@ $(document).ready(function () {
     // slide to annotation list
 
     $(".view-annotation").on('click', function (e) {
+        var href = $(this).attr('href');
+        $(".category-wrap").show();
+        $(".cluster-wrap").show();
 
         // prevent default anchor click behavior
         e.preventDefault();
-
-        // animate
         $('html, body').animate({
             scrollTop: $(this.hash).offset().top
         }, 300, function () {
-
-            // when done, add hash to url
-            // (default click behaviour)
-            window.location.hash = this.hash;
+            window.location.hash = href;
         });
 
     });
@@ -77,18 +77,16 @@ $(document).ready(function () {
     // slide to annotation list
 
     $(".view-annotation-category").on('click', function (e) {
-
-        // prevent default anchor click behavior
+        var href = $(this).attr('href');
         e.preventDefault();
+        $('.cluster-wrap').show();
+        $('.view-annotation-cluster').removeClass('active');
+        $('.annotation-category-cluster li:first-child a').addClass('active');
 
-        // animate
         $('html, body').animate({
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top-25
         }, 300, function () {
-
-            // when done, add hash to url
-            // (default click behaviour)
-            window.location.hash = this.hash;
+            window.location.hash = href;
         });
 
     });
@@ -97,18 +95,22 @@ $(document).ready(function () {
 
     $('#search-form:not(.search-page-form) input[type="text"]').focus(function () {
         $('.search-input-wrapper').show();
+        $('.contract-number-wrap').hide();
 
     });
 
     $(document).on('click', '.search-close', function () {
         $('.search-input-wrapper').hide();
+        $('.contract-number-wrap').show();
     });
 
     $('#search-form input[type="text"]').keyup(function () {
         if ($(this).val() == '') {
             $('.search-input-wrapper').hide();
+            $('.contract-number-wrap').show();
         } else {
             $('.search-input-wrapper').show();
+            $('.contract-number-wrap').hide();
         }
     });
 
@@ -121,7 +123,7 @@ $(document).ready(function () {
         $('#search-form input[type="text"]').focus();
     });
 
-    if($(window).width() > 992) {
+    if ($(window).width() > 992) {
         var annotationWrapHeight = $('.col-lg-6 .panel-contract-wrap').height();
         $('.panel-annotation-wrap').css('height', annotationWrapHeight);
     }
@@ -129,8 +131,8 @@ $(document).ready(function () {
         $('.panel-annotation-wrap').css('height', 'auto');
     }
 
-    $(window).on('resize',function(){
-        if($(window).width() > 992) {
+    $(window).on('resize', function () {
+        if ($(window).width() > 992) {
             var annotationWrapHeight = $('.col-lg-6 .panel-contract-wrap').height();
             $('.panel-annotation-wrap').css('height', annotationWrapHeight);
         }
@@ -139,13 +141,13 @@ $(document).ready(function () {
         }
     });
 
-    if($(window).width() > 992) {
+    if ($(window).width() > 992) {
         var countryLine = $('.country-detail-wrapper').height();
         $('.country-contract-wrap').css('height', countryLine);
     }
 
-    $(window).on('resize',function(){ 
-        if($(window).width() > 992) {
+    $(window).on('resize', function () {
+        if ($(window).width() > 992) {
             var countryLine = $('.country-detail-wrapper').height();
             $('.country-contract-wrap').css('height', countryLine);
         }
@@ -153,15 +155,15 @@ $(document).ready(function () {
 
     $("#no-pin-message").each(function () {
         if (!$(this).text().trim().length) {
-            $(this).parents('.pin-list').css('display','none');
+            $(this).parents('.pin-list').css('display', 'none');
         }
     });
 
-    $(document).click(function(e){
-        if(!$(e.target).closest('.download-wrap, .dropdown-menu').length){
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.download-wrap, .dropdown-menu').length) {
             $(".dropdown-menu").hide();
         }
-         if(!$(e.target).closest('.view-pin-wrap, #pinLists').length){
+        if (!$(e.target).closest('.view-pin-wrap, #pinLists').length) {
             $("#pinLists").hide();
         }
     });
@@ -180,9 +182,25 @@ $(document).ready(function () {
 
     $(".sidebar-nav li ul").each(function () {
         if (!$(this).text().trim().length) {
-            $(this).parent('li').css('display','none');
+            $(this).parent('li').css('display', 'none');
         }
     });
+
+
+    $(document).on('click', '.view-annotation-cluster', function (e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        $('.annotation-category-cluster .active').removeClass('active');
+
+        $(this).addClass('active');
+
+        if (href == '#cluster-all') {
+            $('.cluster-wrap').show();
+        } else {
+            $('.cluster-wrap').hide();
+            $(href).show();
+        }
+    })
 
 
 });
--- public/scripts/contract.view.custom/views/metadata.view.js
+++ public/scripts/contract.view.custom/views/metadata.view.js
@@ -1,58 +1,92 @@
 var MetadataToggleButton = React.createClass({
+    getInitialState: function() {
+        return {
+            showMeta: true
+        }
+    },
     handleClick: function() {
-        if(!this.props.contractApp.get("showMeta")) {
-            this.props.contractApp.set({showMeta: true});
-            location.hash="/meta/show";
-            $('.metadata-toggle-button').addClass("metadata-shown");
-            $('.pdf-viewer').css("width","50%");
-            $(".text-annotator").css("width","50%");
+        if(this.state.showMeta) {
+            this.setState({showMeta: false});
+            // this.props.contractApp.set({showMeta: true});
+            $('.right-column-view').hide();
+            $('.metadata-toggle-button').removeClass("metadata-shown");
+            $('.pdf-viewer').css("width","70%");
+            $(".text-panel").css("width","70%");
         }
         else {
-            this.props.contractApp.set({showMeta: false});
-            location.hash="/meta/hide";
-            $('.metadata-toggle-button').removeClass("metadata-shown");
-            $('.pdf-viewer').css("width","72%");
-            $(".text-annotator").css("width","72%");
+            // this.props.contractApp.set({showMeta: false});
+            // location.hash="/meta/hide";
+            this.setState({showMeta: true});
+            $('.metadata-toggle-button').addClass("metadata-shown");
+            $('.right-column-view').show();            
+            $('.pdf-viewer').css("width","50%");
+            $(".text-panel").css("width","50%");
         }
     },
     render: function() {
         return (
-            <div className="metadata-toggle-button pull-right ">
+            <div className="metadata-toggle-button pull-right metadata-shown">
                 <span onClick={this.handleClick}>Meta</span>
             </div>
         );
     }
 });
+
 var MetadataView = React.createClass({
+    getInitialState: function() {
+        return {
+            showMoreMetadata: false
+        }
+    },
     componentDidMount: function() {
         var self = this;
         this.props.metadata.on("sync", function() {
             self.forceUpdate();
         });
-    }, 
+    },
+    clickShowMoreMetadata: function(e) {
+        e.preventDefault();
+        this.setState({showMoreMetadata: !this.state.showMoreMetadata});
+        if(!this.state.showMoreMetadata) {
+            $(".metadata-view .show-more-meta").show(500);
+        } else {
+            $(".metadata-view .show-more-meta").hide(500);
+        }
+    },
     render: function() {
+        var showLabel = "Show more";
+        if(this.state.showMoreMetadata) {
+            showLabel = "Show less";
+        }
         if(this.props.metadata.get("country")) {
             return (
                 <div className="metadata-view">
-                    <div>Metadata</div>
+                    <div>
+                        Metadata
+                        <div className="metadata-view-footer pull-right">
+                            <a href={this.props.contractApp.getMetadataSummaryLink()}>See Summary</a>
+                        </div>
+                    </div>
+
                     <div className="metadata-country">
                         <span>Country</span>
                         <span>{this.props.metadata.get("country").name}</span>
                     </div>
                     <div className="metadata-signature-year">
                         <span>Signature year</span>
-                        <span>{this.props.metadata.get("signature_year")}</span>
+                        <span>{this.props.metadata.get("signature_year") || "-"}</span>
+                    </div>
+                    <div className="metadata-resource">
+                        <span>Resources</span>
+                        <span>{this.props.metadata.get("resource").join(", ")}</span>
                     </div>
                     <div className="metadata-type-contract">
                         <span>Type of Contract</span>
                         <span>{this.props.metadata.get("type_of_contract")}</span>
                     </div>
-                    <div className="metadata-resource">
-                        <span>Resources</span>
-                        <span>{this.props.metadata.get("resource").join(",")}</span>
-                    </div>
-                    <div>
-                    <a href={this.props.contractApp.getMetadataSummaryLink()}>See details</a>
+                    <div className="metadata-ocid">
+                        <span>Open Contracting Identifier</span>
+                        <span>{this.props.metadata.get("open_contracting_id")}</span>
                     </div>
                 </div>
             );            
@@ -60,7 +94,10 @@ var MetadataView = React.createClass({
             return (
                 <div className="metadata-view">
                     <div>Metadata</div>
-                    <span>Loading...</span>
+                    <span>Loading... </span>
+                    <div className="metadata-view-footer">
+                        <a href={this.props.contractApp.getMetadataSummaryLink()}>See summary</a>
+                    </div>
                 </div>
             );
         }
@@ -69,23 +106,159 @@ var MetadataView = React.createClass({
 });
 
 var RelatedDocumentsView = React.createClass({
+    componentDidMount: function() {
+        var self = this;
+        this.props.metadata.on("sync", function() {
+            self.forceUpdate();
+        });
+    },
     render: function() {
-        return (
-            <div className="relateddocument-view">            
-                
-            </div>
-        );        
+        function truncate(text) {
+            var words = (text + "").split(" ");
+            var ellipsis = ""
+            if(words.length > 10) {
+                ellipsis = " ...";
+            }
+            words = words.splice(0, 10);
+            return words.join(" ") + ellipsis;
+        }        
+        var parentContracts = "", 
+            supportingContracts = []
+            moreContracts = "";
+        if(this.props.metadata.get("parent_document")) {
+            parentContracts = this.props.metadata.get("parent_document").map(function(doc) {
+                var docUrl = app_url + "/contract/" + doc.id;
+                if(doc.status === "published") {
+                    return (
+                        <span>
+                            <a href={docUrl}>{doc.contract_name}</a>
+                        </span>
+                    );
+                } else {
+                    return (
+                        <span>
+                            {doc.contract_name}
+                        </span>
+                    );
+                }
+            });
+            var MaxAllowed = 2;
+            var maxDocs = (this.props.metadata.get("supporting_contracts").length < MaxAllowed)?this.props.metadata.get("supporting_contracts").length:MaxAllowed;
+            for(var i = 0;i < maxDocs; i++) {
+                var doc = this.props.metadata.get("supporting_contracts")[i];
+                if(doc.status === "published") {
+                    var docUrl = app_url + "/contract/" + doc.id;
+                    supportingContracts.push(<span id={i}><a href={docUrl}>{truncate(doc.contract_name)}</a></span>);
+                } else {
+                    supportingContracts.push(<span id={i}>{truncate(doc.contract_name)}</span>);
+                }
+            }
+            if(this.props.metadata.get("supporting_contracts").length > MaxAllowed) {
+                moreContracts = (<span><a href={this.props.contractApp.getMetadataSummaryLink() + "#relateddocs"}>All related ...</a></span>);
+            }
+            if(parentContracts.length || supportingContracts.length) {
+                return (
+                    <div className="relateddocument-view">
+                        <div>Related docs</div>
+                        {parentContracts}
+                        {supportingContracts}
+                        {moreContracts}
+                    </div>
+                );
+            } else {
+                return (<div></div>);
+            }
+        } else {
+            return (
+                <div className="relateddocument-view">
+                    <div>Related docs</div>
+                    Loading...
+                </div>
+            );
+        }
+
     }
 });
+var RelatedDocumentsMoreView = React.createClass({
+    componentDidMount: function() {
+        var self = this;
+        this.props.metadata.on("sync", function() {
+            self.forceUpdate();
+        });
+    },
+    render: function() {
+        if(this.props.metadata.get("country")) {
+            var countryCode = this.props.metadata.get("country").code.toLowerCase();
+            var countryLink = app_url + "/countries/" + countryCode;
+            var country = React.createElement('a', {href: countryLink}, this.props.metadata.get("country").name);
+            var resourceLinkBase = app_url + "/resources/";
+            var resources = this.props.metadata.get("resource").map(function(resource, i) {
+                return React.createElement('a', {href: app_url + "/resource/" + resource, key: i}, resource);
+            });
+            return (
+                <div className="relateddocument-more-view">
+                    <div>More</div>
+                    <div>
+                        <div>From {country}</div>
+                        <div>For {resources}</div>
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div className="relateddocument-more-view">
+                    <div>More</div>
+                    <span>Loading...</span>
+                </div>
+            );
+        }
+    }
+});
+var OtherSourcesView = React.createClass({
+    componentDidMount: function() {
+        var self = this;
+        this.props.metadata.on("sync", function() {
+            self.forceUpdate();
+        });
+    },
+    render: function() {
+        if(this.props.metadata.get("company")) {
+            var amla_url = this.props.metadata.get("amla_url");
+            var amlaUrlLink = (<span><a href={amla_url}>{this.props.metadata.get("country").name}</a> Legislation</span>);
 
+            if(amla_url) {
+                return (
+                    <div className="other-sources-view">
+                        <div>Other Sources</div>
+                        <div>
+                            <div>{amlaUrlLink}</div>
+                        </div>
+                    </div>
+                );                
+            } else {
+                return (<div></div>);
+            }
+        } else {
+            return (
+                <div className="other-sources-view">
+                    <div>Other Sources</div>
+                    <span>Loading...</span>
+                </div>
+            );
+        }
+    }
+});
 var RightColumnView = React.createClass({
     render: function() {
         return (
-            <div className="right-column-view" style={this.props.style}>
+            <div className="right-column-view">
                 <MetadataView 
                     contractApp={this.props.contractApp}
                     metadata={this.props.metadata} />
                 <RelatedDocumentsView
+                    contractApp={this.props.contractApp}
+                    metadata={this.props.metadata} />
+                <RelatedDocumentsMoreView
                     metadata={this.props.metadata} />
             </div>
         );
