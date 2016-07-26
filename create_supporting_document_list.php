c70fdb736aebc8fc803f690a116666c7a97610c1
Thea <raebaraquio@gmail.com>
2016-07-26 22:30:24 +0800

    Restore API call

--- create_supporting_document_list.php
+++ create_supporting_document_list.php
@@ -3,13 +3,10 @@
 $url = "http://api.resourcecontracts.org/contracts/search?q=&from=0&per_page=10000&group=metadata&country=ph";
 */
 $url = "http://rc-api-stage.elasticbeanstalk.com/api/contracts/search?q=&from=0&per_page=10000&group=metadata&country_code=ph";
-$raw = file_get_contents('all_results.json');
-
+$raw = file_get_contents($url);//all_results.json
 $json = json_decode($raw); 
 $results = $json->results;
-
 $supporting_documents = array();
-
 foreach ($results as $r) {
 	$contract_url = "http://rc-api-stage.elasticbeanstalk.com/api/contract/" . $r->id . "/metadata";
 	$raw = file_get_contents($contract_url);
@@ -18,9 +15,7 @@ foreach ($results as $r) {
 		array_push($supporting_documents, $json->id);
 	}
 }
-
 $written = file_put_contents('supporting_documents.js', "supporting_documents = [" . implode(',', $supporting_documents) . "]");
-
 if ($written) {
   echo "Updated";
 }
