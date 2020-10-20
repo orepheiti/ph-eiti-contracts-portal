3611bfa651f0157c97db821d7465c40f342a87ec
ORE <ore.ph.site@gmail.com>
2020-10-20 21:36:28 +0800

    Synced prod files

---
+++ mailtest.php
@@ -0,0 +1,7 @@
+<?php
+
+$subject = "PH-EITI Contracts Portal - Feedback";
+$message = "Hi \r\n, Test";
+$headers = 'From: jerico@outlook.ph' . "\r\n";
+
+mail('jerico@gmail.com', $subject, $message, $headers);
